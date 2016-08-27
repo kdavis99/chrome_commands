6ecb8a4ad000a1c5e9c1d381e070177941b239bd
kdavis99 <kdavis99@mail.csuchico.edu>
2016-08-27 15:04:55 -0700

    making duplicate tabs with a range (/) works

--- popup.js
+++ popup.js
@@ -1,15 +1,23 @@
 window.onload = function() {
-	function user_command() {
-		var input = document.getElementById('user_input').value;
-		var res = input.split("/");
-			alert(res[0]);
-			alert(res[1]);
-			chrome.tabs.query({currentWindow: true}, function (arrayOfTabs) {
-				for (i = res[0]; i <= res[1]; i++) {
-				    chrome.tabs.duplicate(arrayOfTabs[i].id);
-				}
-			});
-		alert(input);
-	}
-	document.getElementById('user').onclick = user_command;
+   function user_command() {
+      var input = document.getElementById('user_input').value;
+      var input_by_space = input.split(" ");
+
+      for (s in input_by_space) {
+         if (input_by_space[s].includes("/")) {
+            var range = input_by_space[s].split("/");
+            var first = range[0];
+            var second = range[1];
+         }
+         else if (input_by_space[s].includes("dup")) {
+            chrome.tabs.query({currentWindow: true}, function (arrayOfTabs) {
+               for (i = first; i <= second; i++) {
+                  chrome.tabs.duplicate(arrayOfTabs[i].id);
+               }
+            });
+         }
+      }
+      
+   }
+   document.getElementById('user').onclick = user_command;
 }
