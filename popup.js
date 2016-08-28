5aeb4e2e67c4e8363890c5572f665f95643b84b3
kdavis99 <kdavis99@mail.csuchico.edu>
2016-08-27 22:40:10 -0700

    multiple cp commands are working for tabs. Delimited by a semi-colon

--- popup.js
+++ popup.js
@@ -1,23 +1,32 @@
 window.onload = function() {
-   function user_command() {
-      var input = document.getElementById('user_input').value;
-      var input_by_space = input.split(" ");
+   function parse_command() {
+      chrome.tabs.query({currentWindow: true}, function (arrayOfTabs) {
+         var all_tabs = new Array();
+         for (i = 0; i < arrayOfTabs.length; i++) {
+            all_tabs[i] = arrayOfTabs[i];
+         }
+         var input = document.getElementById('user_input').value;
+         var input_commands = input.split(";");
 
-      for (s in input_by_space) {
-         if (input_by_space[s].includes("/")) {
-            var range = input_by_space[s].split("/");
-            var first = range[0];
-            var second = range[1];
-         }
-         else if (input_by_space[s].includes("dup")) {
-            chrome.tabs.query({currentWindow: true}, function (arrayOfTabs) {
-               for (i = first; i <= second; i++) {
-                  chrome.tabs.duplicate(arrayOfTabs[i].id);
+         for (command in input_commands) {
+            var command_by_space = input_commands[command].split(" ");
+
+            for (str in command_by_space) {
+               if (command_by_space[str].includes("/")) {
+                  var range = command_by_space[str].split("/");
+                  var first = range[0];
+                  var second = range[1];
                }
-            });
+               if (command_by_space[str] == "cp") {
+                  for (i = first - 1; i < second; i++) {
+                     if (arrayOfTabs[i + 1]) {
+                        chrome.tabs.duplicate(arrayOfTabs[i + 1].id);
+                     }
+                  }
+               }
+            }
          }
-      }
-      
+      });
    }
-   document.getElementById('user').onclick = user_command;
+   document.getElementById('user').onclick = parse_command;
 }
