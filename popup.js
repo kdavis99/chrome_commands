6b0d1acc2821b5c486c90b5fafd66291bfae7ee9
kdavis99 <kdavis99@mail.csuchico.edu>
2016-08-29 22:53:54 -0700

    added reloading + iterating through all tabs and removing duplicates

--- popup.js
+++ popup.js
@@ -33,55 +33,91 @@ window.onload = function() {
       var command_by_space = command.split(" ");
       console.log(command);
 
+      var first = -1, second = -1;
       for (str in command_by_space) {
          word = trim(command_by_space[str]);
          console.log(word);
          if (word.includes("/")) {
+            console.log("/");
             var range = word.split("/");
             var first = range[0];
             var second = range[1];
-            // if "5/" -- the range is only 5
-            if (!range[1]) {
-               second = first;
-            }
-         } else if (word == "cp" || word == "rm") {
+         } else if ((first > -1 && second > -1) && (word == "yy" || word == "dd")) {
+            console.log("yyrdd");
             // potential "hacky" solution - however it seemed to be more
             // of a bug in chrome.tabs.duplicate
             for (i = first - 1; i < second; i++) {
                if (all_tabs[i + 1]) {
                   switch(word) {
-                     case "cp":
-                        console.log("cp");
+                     case "yy":
+                        console.log("fyy" + (i + 1));
+                        console.log(all_tabs[i+1].id);
                         chrome.tabs.duplicate(all_tabs[i + 1].id);
                         break;
-                     case "rm":
-                        console.log("rm");
+                     case "dd":
+                        console.log("fdd");
                         chrome.tabs.remove(all_tabs[i + 1].id);
                         break;
                   }
                }
             }
-         } else if (word.includes("dd") || word.includes("yy")) {
+         } else if (word == "rmdup") {
+            // remove all tabs with a duplicate title
+            var count_map = {};
+            for (tab in all_tabs) {
+               console.log(all_tabs[tab].title);
+               if (count_map[all_tabs[tab].title] != 1) {
+                  count_map[all_tabs[tab].title] = 1;
+               } else {
+                  chrome.tabs.remove(all_tabs[tab].id);
+               }
+            }
+         } else if (/[0-9a-z]/i.test(word)) {
             console.log("yydd");
             console.log(command_by_space[++str]);
+            found_num = false;
             for (tabs in map_tabs) {
-               // toLowerCase to enforce normalization
-               if (tabs.includes(command_by_space[str].toLowerCase())) {
-                  switch(word) {
-                     case "yy":
-                        console.log("yy");
-                        chrome.tabs.duplicate(map_tabs[tabs].id);
-                        break;
+               if (!found_num && map_tabs[tabs] && (map_tabs[tabs].url.includes(word.toLowerCase()) ||
+                        tabs.includes(word.toLowerCase()) || /[0-9]/.test(word))) {
+                  switch(command_by_space[str]) {
                      case "dd":
-                        console.log("dd");
-                        chrome.tabs.remove(map_tabs[tabs].id);
+                     {
+                        if (/[0-9]/.test(word)) {
+                           chrome.tabs.remove(all_tabs[word].id);
+                           found_num = true;
+                        } else {
+                           console.log(map_tabs[tabs]);
+                           chrome.tabs.remove(map_tabs[tabs].id);
+                        }
                         break;
+                     }
+                     case "rr":
+                     {
+                        console.log("rr");
+                        if (/[0-9]/.test(word)) {
+                           chrome.tabs.reload(all_tabs[word].id);
+                           found_num = true;
+                        } else {
+                           chrome.tabs.reload(map_tabs[tabs].id);
+                        }
+                        break;
+                     }
+                     case "yy":
+                     {
+                        console.log("yy");
+                        // check if deleting index (number) or title
+                        if (/[0-9]/.test(word)) {
+                           chrome.tabs.duplicate(all_tabs[word].id);
+                           found_num = true;
+                        } else {
+                           chrome.tabs.duplicate(map_tabs[tabs].id);
+                        }
+                        break;
+                     }
                   }
                }
             }
          }
-         // add deleting all duplicate tabs
-         // do reload
       }
    }
    document.getElementById('user').onclick = init;
